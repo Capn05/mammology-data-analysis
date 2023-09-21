2193539ab049ea5d64d374955b28aab7c20c2db0
Capn05 <89543708+Capn05@users.noreply.github.com>
2023-09-21 13:46:23 -0500

    Update Workshop.py

--- Workshop.py
+++ Workshop.py
@@ -45,6 +45,7 @@ states1 = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE","DC", "FL", "GA",
           "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", 
           "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
 
+# Create a dictionary for integration into a dataframe
 data = {'State': states1, 'Critical Population': states_weighted_pops}
 print(states_weighted_pops)
 # Convert the dictionary to a data frame
@@ -54,11 +55,6 @@ df = pd.DataFrame(data)
 print(df)
 print(states_weighted_pops)
 
-
-# In[281]:
-
-
-
 critical_population = np.array([920767.487, 114800.397, 1269903.815, 542352.387, 7034553.961999999, 1000686.8869999999, 696460.6610000001, 182495.152, 111414.833, 4126265.0549999997, 1966310.4759999998, 252603.30700000003, 300490.137, 2338461.5810000002, 1206607.2610000002, 552294.3600000001, 499323.34699999995, 822894.488, 827395.61, 265963.226, 1153973.283, 1304706.368, 1846747.443, 995383.6830000001, 545360.551, 1112945.055, 186061.232, 326061.52, 546401.721, 263906.497, 1730427.1600000001, 365019.82699999993, 3656160.2600000002, 1973340.3490000002, 119486.242, 2152930.3, 682135.278, 762064.54, 2401654.779, 200421.92500000002, 959038.609, 146788.785, 1267199.514, 4980634.13, 481879.684, 118205.642, 1577975.533, 1335880.942, 342052.962, 1056509.483, 97662.37200000002])
 facilities_per_state = np.array([142, 33, 171, 82, 761, 138, 114, 30, 16, 628, 277, 37, 49, 335, 179, 145, 109, 144, 145, 48, 132, 155, 271, 210, 99, 159, 47, 93, 69, 44, 247, 46, 550, 270, 48, 333, 116, 108, 343, 34, 110, 48, 192, 568, 64, 15, 205, 149, 64, 215, 30])
 
@@ -67,6 +63,7 @@ plt.xlabel("Critical Population")
 plt.ylabel("Facilities")
 plt.plot()
 
+# Perform linear regression and print slope and intercept
 slope, intercept = np.polyfit(critical_population, facilities_per_state, deg=1)
 plt.plot(critical_population, (slope*critical_population)+intercept)
 print("slope = " + str(slope))
@@ -81,7 +78,8 @@ distances = []
 for i in range(len(critical_population)):
     distance = facilities_per_state[i] - (slope*critical_population[i]+intercept)
     distances.append(distance)
-    
+
+# Calculate the sum of negative distances    
 state_to_distance = {}
 for i in range(len(distances)):
     state_to_distance[states[i]] = distances[i]
@@ -95,13 +93,14 @@ sum = 0
 for num in neg_dict.values():
     sum += num
 print(sum)
-
+# Calculate proportions of negative distances and store them in a dictionary
+pps = {}
 pps = {}
 for state,value in neg_dict.items():
     portion = value/sum
     pps[state]= [portion]
 print(pps)
-
+# Create a data frame from the proportions dictionary
 percentages = pd.DataFrame(pps)
 
 
@@ -112,12 +111,12 @@ percentages = pd.DataFrame(pps)
 # In[157]:
 
 
-percentages
+print(percentages)
 
 
 # In[282]:
 
-
+# Plot the proportions using squarify
 import squarify
 import matplotlib.pyplot as plt
 keys, values = [], []
@@ -132,8 +131,7 @@ print(regular_list)
     
 print(keys)
 print(regular_list)
-import squarify
-import matplotlib.pyplot as plt
+# Define labels for the plot
 
 # Data
 values = [0.017244338881954216, 0.014796699730856696, 0.018919087659749, 0.1628750591410145, 0.018941499476253666, 0.03349027815068756, 0.04210334876072615, 0.03445002111389311, 0.026280143546200313, 0.020581065317085097, 0.05445681763547917, 0.044197762375366934, 0.003468492699833141, 0.00797304299239199, 0.04150495116658301, 0.026798853042252036, 0.0420020183426251, 0.019431262927606583, 0.03012614057245279, 0.052821208607063366, 0.09464560337761832, 0.037470044015556425, 0.044704468587089624, 0.02044593988465688, 0.062255245405372354, 0.00987077433918479, 0.01814583225044701]
